039358dab1a88621f402f99b05956a735295daa8
Dmytro Yefremenko <mitrandir.white@gmail.com>
2022-05-15 18:21:20 +0300

    Slider initialised

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import './sass/main.scss';
+import './sass/main.css';
 
 
 $(document).ready(function(){
   $('.reviews__slider').slick({
-
+    slidesToShow: 1,
+    slidesToScroll: 1
   });
-});
+  $('.reviews__slider--nav').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    asNavFor: '.reviews__slider',
+    centerMode: false,
+    focusOnSelect: true,
+    variableWidth: 54
+  });
+  });
