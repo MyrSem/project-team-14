87927027f00247148a717f1fb990e160e195c008
Dmytro Yefremenko <mitrandir.white@gmail.com>
2022-05-16 01:13:05 +0300

    + mobil CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+
+$(document).ready(function(){
+  $('.reviews__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  $('.reviews__slider--nav').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    asNavFor: '.reviews__slider',
+    centerMode: false,
+    focusOnSelect: true,
+    variableWidth: 54
+  });
+  });
