8956772306ae85be03d04823ce44d3d8ff5e8309
Myroslav Semeniuk <97482927+MyrSem@users.noreply.github.com>
2022-05-18 16:27:18 +0300

    fix mobile-menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,18 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector('[data-menu-open]'),
-      closeMenuBtn: document.querySelector('[data-menu-close]'),
-      menu: document.querySelector('[data-menu]'),
-    };
-  
-    refs.openMenuBtn.addEventListener('click', toggleModal);
-    refs.closeMenuBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.menu.classList.toggle('is-open');
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
+  document.querySelectorAll(".mobile-menu__link").forEach(link => link.addEventListener('click', closeModal))
+const closeModal = () => {
+	refs.menu.classList.remove("is-open");
+}
+})();
